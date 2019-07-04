62c0b800bac3fece3a149a1376f6164afc3e55cf
921595360@qq.com <19940808aB>
2019-07-04 23:03:00 +0800

    add hystrix

--- springCloud-demo/consumer/src/main/java/com/jiawa365/consumer/controller/TestController.java
+++ springCloud-demo/consumer/src/main/java/com/jiawa365/consumer/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.jiawa365.consumer.controller;
 
 import com.jiawa365.consumer.service.TestService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +16,14 @@ public class TestController {
     @Autowired
     private TestService testService;
 
+    @HystrixCommand(fallbackMethod = "error")
     @RequestMapping("test")
     @ResponseBody
     public String test(){
         return testService.test();
+    }
+
+    public String error(){
+        return "error,wait moment";
     }
 }
